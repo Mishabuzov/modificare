b4bb5cff067ee5ba8be4969d611fef5d03905ef4
Mikhail Abyzov <mikhaijl7@gmail.com>
2020-05-05 12:55:49 -0700

    rewrite runner.R to make all the algorithms starts n times

--- runner.R
+++ runner.R
@@ -38,24 +38,12 @@ run_greedy_n_times <- function (times) {
   }
 }
 
-run_greedy <- function () {
-  print("run greedy algorithm:")
-  y <- GRD(COV_MATRIX_PATH, TIMING_FILE)
-  save_ordering(y, "greedy_ordering.txt")
-}
-
 run_GA_n_times <- function (times) {
   for (seed in 1:times) {
     print(paste("Testing GA with seed:", seed))
     y <- GA(x, Seed=seed)
     save_ordering(y, sprintf("genetic_ordering_%s.txt", seed))
   }
-}
-
-run_GA <- function () {
-  print("Running GA algorithm.")
-  y <- GA(x)
-  save_ordering(y, "genetic_ordering.txt")
 }
 
 run_HC_FA_n_times <- function (times) {
@@ -66,22 +54,20 @@ run_HC_FA_n_times <- function (times) {
   }
 }
 
-run_HC_FA <- function () {
-  print("Running HC_FA algorithm.")
-  y <- HC_FA(x)
-  save_ordering(y, "HC_FA_ordering.txt")
+run_ART_n_times <- function (times) {
+  for (seed in 1:times) {
+    print(paste("Testing ART with seed:", seed))
+    y <- ART(read.table(COV_MATRIX_PATH), "ManhattanDistance", "avg", seed=seed)
+    save_ordering(y, sprintf("ART_man_avg_ordering_%s.txt", seed))
+  }
 }
 
-run_ART <- function () {
-  print("run ART (dist=Manh, avg).")
-  y <- ART(read.table(COV_MATRIX_PATH), "ManhattanDistance", "avg")
-  save_ordering(y, "ART_man_avg_ordering.txt")
-}
-
-run_HC_SA <- function () {
-  print("Running HC_SA algorithm.")
-  y <- HC_SA(x)
-  save_ordering(y, "HC_SA_ordering.txt")
+run_HC_SA_n_times <- function (times) {
+  for (seed in 1:times) {
+    print(paste("Testing HC_SA with seed:", seed))
+    y <- HC_SA(x, Seed=seed)
+    save_ordering(y, sprintf("HC_SA_ordering_%s.txt", seed))
+  }
 }
 
 # Error in HC_RA_real(lFM, NG, Ord, Seed) (FDP_HC.R#352): object 'ItLimit' not found
@@ -91,13 +77,15 @@ run_HC_SA <- function () {
 #   save_ordering(y, "HC_RA_ordering.txt")
 # }
 
-run_SANN <- function () {
-  print("Running SANN algorithm.")
-  y <- SANN(x)
-  save_ordering(y, "SANN_ordering.txt")
+run_SANN_n_times <- function (times) {
+  for (seed in 1:times) {
+    print(paste("Testing SANN with seed:", seed))
+    y <- SANN(x, Seed=seed)
+    save_ordering(y, sprintf("SANN_ordering_%s.txt", seed))
+  }
 }
 
-PROJECT_NAMES <- c("chart", "lang", "time", "math", "closure")
+PROJECT_NAMES <- c("closure")
 n <- 5
 init()
 for (PROJECT_NAME in PROJECT_NAMES) {
@@ -105,14 +93,5 @@ for (PROJECT_NAME in PROJECT_NAMES) {
   COV_MATRIX_PATH <- sprintf("reqMatrices/D4J_method_matrices/%s_method_coverage_matrix.txt", PROJECT_NAME)
   TIMING_FILE <- sprintf("reqMatrices/TimingsFiles/%s_id_name_mapping.txt", PROJECT_NAME)
   x <- makeLogFM(read.table(COV_MATRIX_PATH))
-  run_HC_FA_n_times(n)
-  run_greedy_n_times(n)
-  # run_random_n_times(100)
-  # run_GA()
-  # run_ART()
-  # run_greedy()
-  # run_HC_SA()
-  # run_HC_FA()
-  # run_HC_RA()
-  # run_SANN()
+  run_ART_n_times(n)
 }
